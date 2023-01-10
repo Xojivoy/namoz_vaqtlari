11cb85fc5a2a9271ab3cb36269a3f3dbd02e739b
Xojivoy <111102503+Xojivoy@users.noreply.github.com>
2023-01-10 10:11:47 +0500

    Update namoz_vaqt.py
    Yangi usul

--- handlers/users/namoz_vaqt.py
+++ handlers/users/namoz_vaqt.py
@@ -2,307 +2,51 @@ import requests
 from datetime import datetime
 from loader import dp
 from aiogram.types import CallbackQuery
-import json
 x = datetime.now()
 
-@dp.callback_query_handler(text='nam')
-async def nami(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/namangan/daily/{x.day}-{x.month}-{x.year}/true/7.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"7","location":"namangan"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Namangan\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='and')
-async def andi(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/andijon/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"andijon"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Andijon\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='tos')
-async def tosi(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/tashkent/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"tashkent"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Toshkent\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='far')
-async def fari(call : CallbackQuery):
-    # # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    # url = f"https://muslimsalat.com/fargona/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    # querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"fargona"}
-
-    # headers = {
-    #     "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-    #     "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"Viloyat : Farg'ona\n"
-    txt+= f"Afsuski bu viloyat bo'yicha so'rovnoma topilmadi."
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='bux')
-async def buxi(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/buxoro/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"buxoro"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Toshkent\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='sur')
-async def suri(call : CallbackQuery):
-    # # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    # url = f"https://muslimsalat.com/surxondaryo/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    # querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"surxondaryo"}
-
-    # headers = {
-    #     "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-    #     "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"Viloyat : Surxondaryo"
-    txt+= f"Afsuski bu viloyat bo'yicha so'rovnoma topilmadi."
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='sir')
-async def siri(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/sirdaryo/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"sirdaryo"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Sirdaryo\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='jiz')
-async def jizi(call : CallbackQuery):
-    # # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    # url = f"https://muslimsalat.com/jizzax/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    # querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"jizzax"}
-
-    # headers = {
-    #     "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-    #     "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"Viloyat : Jizzax"
-    txt+= f"Afsuski bu viloyat bo'yicha so'rovnoma topilmadi."
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='qas')
-async def qasi(call : CallbackQuery):
-    # # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    # url = f"https://muslimsalat.com/qashqadaryo/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    # querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"qashqadaryo"}
-
-    # headers = {
-    #     "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-    #     "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"Viloyat : Qashqadaryo"
-    txt+= f"Afsuski bu viloyat bo'yicha so'rovnoma topilmadi."
-
-    await call.message.answer(txt)
+@dp.callback_query_handler()
+async def nami(call:CallbackQuery):
+    callll = call.get_current()
+    data = callll['data']
+    url = f"https://dailyprayer.abdulrcs.repl.co/api/{data}"
     
-
-@dp.callback_query_handler(text='sam')
-async def sami(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/samarqand/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"samarqand"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Samarqand\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
+    response = requests.get(url)
+    datas = response.json()
+    xufton  = datas['today']['Isha\'a']
+    xufton1  = datas['tomorrow']['Isha\'a']
+    osha = datas['today']['Asr']
+    a,b = osha.split(":")
+    a = int(a)
+    b = int(b)
+    y = a*60 + 40 + b
+    d = y // 60
+    e = y % 60
+    osha1 = datas['tomorrow']['Asr']
+    a1,b1 = osha1.split(":")
+    a1 = int(a1)
+    b1 = int(b1)
+    y1 = a1*60 + 40 + b1
+    d1 = y1 // 60
+    e1 = y1 % 60
+    txt = "❗️❕❗️Quyidagilar namoz kirish vaqtlari hisoblanadi❕❗️❕\n" 
+    txt+= f"📍Viloyat : {data}\n"
+    txt+= "\n⁑Bugungilik Namoz vaqtlari :\n"
+    txt+= f"\n📅Kun : {x.day}.{x.month}.{x.year}\n"
+    txt+= f"\n🌄Quyosh chiqishi : {datas['today']['Sunrise']}\n"
+    txt+= f"\n»Bomdod : {datas['today']['Fajr']}\n"
+    txt+= f"»Peshin : {datas['today']['Dhuhr']}\n"
+    txt+= f"»Asr : {d}:{e}\n"
+    txt+= f"»Shom : {datas['today']['Maghrib']}\n"
+    txt+= f"»Xufton : {xufton}"
+    txt1 = "❗️❕❗️Quyidagilar namoz kirish vaqtlari hisoblanadi❕❗️❕\n" 
+    txt1+= "\n⁑Ertangilik Namoz vaqtlari :\n"
+    txt1+= f"\n📅Kun : {x.day}.{x.month}.{x.year}\n"
+    txt1+= f"\n🌄Quyosh chiqishi : {datas['tomorrow']['Sunrise']}\n"
+    txt1+= f"\n»Bomdod : {datas['tomorrow']['Fajr']}\n"
+    txt1+= f"»Peshin : {datas['tomorrow']['Dhuhr']}\n"
+    txt1+= f"»Asr : {d1}:{e1}\n"
+    txt1+= f"»Shom : {datas['tomorrow']['Maghrib']}\n"
+    txt1+= f"»Xufton : {xufton1}"
     await call.message.answer(txt)
-
-@dp.callback_query_handler(text='nav')
-async def navi(call : CallbackQuery):
-    # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    url = f"https://muslimsalat.com/navoiy/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"navoiy"}
-
-    headers = {
-        "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-        "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"📍Viloyat : Navoiy\n"
-    txt+= f"»Bomdod : {dates['items'][0]['fajr']}\n"
-    txt+= f"🌄Quyosh chiqishi : {dates['items'][0]['shurooq']}\n"
-    txt+=f"»Peshin : {dates['items'][0]['dhuhr']}\n"
-    txt+=f"»Asr : {dates['items'][0]['asr']}\n"
-    txt+=f"»Shom : {dates['items'][0]['maghrib']}\n"
-    txt+=f"»Xufton : {dates['items'][0]['isha']}\n"
-    txt+=f"\n🕋Qibla : {dates['qibla_direction']}° da"
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='xor')
-async def xori(call : CallbackQuery):
-    # # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    # url = f"https://muslimsalat.com/xorazm/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    # querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"xorazm"}
-
-    # headers = {
-    #     "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-    #     "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"Viloyat : Xorazm"
-    txt+= f"Afsuski bu viloyat bo'yicha so'rovnoma topilmadi."
-
-    await call.message.answer(txt)
-
-@dp.callback_query_handler(text='qor')
-async def qori(call : CallbackQuery):
-    # # url = "https://muslimsalat.p.rapidapi.com/tashkent/(times)/(date)/(daylight)/(method).json"
-    # url = f"https://muslimsalat.com/qoraqalpogiston/daily/{x.day}-{x.month}-{x.year}/true/5.json"
-    # querystring = {"times":"daily","date":f"{x.day}-{x.month}-{x.year}","method":"5","location":"qoraqalpogiston"}
-
-    # headers = {
-    #     "X-RapidAPI-Key": "f14a1dfd69msh1746c4f381ffc2dp174044jsnd638b758dd13",
-    #     "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # dates=json.loads(response.text)
-    txt = "Bugungi namoz vaqtlari\n"
-    txt+= f"\n📅Kun : {x.day}-{x.month}-{x.year}\n"
-    txt+= f"Viloyat : Qoraqalpog'iston"
-    txt+= f"Afsuski bu viloyat bo'yicha so'rovnoma topilmadi."
-
-    await call.message.answer(txt)
+    await call.message.answer(txt1)
+    # print(datas)
